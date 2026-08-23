50db588d9aee342add91f963ac1b7bdd26d82c83
Arets Paeglis <arets.paeglis@protonmail.com>
2026-08-23 14:32:27 +0300

    Restored research and breeding reactors (sorry, Bobcat, I like them too much)

--- src/main/java/com/hbm/main/NEIRegistry.java
+++ src/main/java/com/hbm/main/NEIRegistry.java
@@ -33,6 +33,7 @@ public class NEIRegistry {
 		handlers.add(new RefineryRecipeHandler());
 		handlers.add(new CentrifugeRecipeHandler());
 		handlers.add(new GasCentrifugeRecipeHandler());
+		handlers.add(new BreederRecipeHandler());
 		handlers.add(new CyclotronRecipeHandler());
 		handlers.add(new VacuumRecipeHandler());
 		handlers.add(new CrackingHandler());
